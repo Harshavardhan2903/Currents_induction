7a98bce08db4deabfb77bb249af7132a7d5ce3a9
Harshavardhan2903 <89390938+Harshavardhan2903@users.noreply.github.com>
2024-12-06 20:21:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const lightningStrike = document.querySelector(".lightning-strike");
+
+    // Trigger the big lightning strike animation on page load
+    lightningStrike.style.animationPlayState = "running";
+});
